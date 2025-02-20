d6b27bf368d211fa0d4edec8da24512e4056e64a
Poitier Stringer <stringerpoitier@gmail.com>
2025-02-19 17:00:45 -0800

    update regex string

--- main.ts
+++ main.ts
@@ -1,134 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class UpdateTaskDueDatesPlugin extends Plugin {
+  async onload() {
+    // Register a command so you can run the update via the Command Palette.
+    this.addCommand({
+      id: 'update-task-due-dates',
+      name: 'Update Task Due Dates to One Year Later',
+      callback: () => this.updateDueDates(),
+    });
+  }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+  async updateDueDates() {
+    // Get all markdown files in the vault.
+    const files = this.app.vault.getMarkdownFiles();
+    console.log(`Found ${files.length} markdown files`);
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    // Regex to match a due date in the format "due: YYYY-MM-DD".
+    /* 📅 2023-08-09 */
+    const dueDateRegex = /📅\s*(\d{4}-\d{2}-\d{2})/g;
+    console.log('Updating due dates...');
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    // Loop through each file.
+    for (const file of files) {
+      let content = await this.app.vault.read(file);
+      console.log(`Checking due dates in ${file.path}`);
+      let hasChange = false;
 
-	async onload() {
-		await this.loadSettings();
+      // Replace each found due date with a new date, one year later.
+      const updatedContent = content.replace(dueDateRegex, (match, dateStr) => {
+        const oldDate = new Date(dateStr);
+        console.log(`Found due date: ${dateStr}`);
+        if (isNaN(oldDate.getTime())) {
+          // If the date is invalid, do not modify it.
+          return match;
+        }
+        // Add one year to the date.
+        oldDate.setFullYear(oldDate.getFullYear() + 1);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // Format the new date as YYYY-MM-DD.
+        const newDateStr = oldDate.toISOString().slice(0, 10);
+        hasChange = true;
+        return `due: ${newDateStr}`;
+      });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      // If any changes were made, write the updated content back to the file.
+      if (hasChange) {
+        await this.app.vault.modify(file, updatedContent);
+        console.log(`Updated due dates in ${file.path}`);
+      }
+    }
+    console.log('Due dates updated!');
+    // Give the user a notification once the process is complete.
+    new Notice('Due dates updated to one year from now!');
+  }
 }
